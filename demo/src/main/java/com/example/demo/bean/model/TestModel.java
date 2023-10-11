6b42987ad73ab0115ea26bb5ed84547a7bfd91a9
Ailes16 <ailes.the16thclass@i.softbank.jp>
2023-10-11 22:04:46 +0900

    spring bootを追加

---
+++ demo/src/main/java/com/example/demo/bean/model/TestModel.java
@@ -0,0 +1,16 @@
+package com.example.demo.bean.model;
+
+import lombok.Data;
+
+/**
+ * Reactの動作確認用のモデルクラス
+ * 
+ * @author Ailes16
+ * @version 1.0
+ */
+@Data
+public class TestModel {
+
+    private String userId = "TEST001";
+    private String userName = "test user";
+}
---
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.bean.model.TestModel;
+
+/**
+ * Reactの動作確認用のコントローラクラス
+ * 
+ * @author Ailes16
+ * @version 1.0
+ */
+@Controller
+@RequestMapping("/")
+public class TestController {
+
+    @ResponseBody
+    @GetMapping
+    public TestModel test() {
+        return new TestModel();
+    }
+}
